ad532b174f91061d5dcb3ac035ccfc9aaaa1101d
Jordy McClarkson <dpmr7n27mm@privaterelay.appleid.com>
2026-01-28 18:09:43 -0500

    Update layout.js

--- app/niragame/layout.js
+++ app/niragame/layout.js
@@ -1,1 +1,16 @@
-
+export default function GameLayout({ children }) {
+  return (
+    <div style={{
+      height: '100dvh',
+      overflow: 'hidden',
+      backgroundColor: '#0a0a0a',
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    }}>
+      {children}
+    </div>
+  );
+}
